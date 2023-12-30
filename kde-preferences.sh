bee892234d863562fa494b7984851bdb0427ebbe
Jzaib <68151478+NewbyJman@users.noreply.github.com>
2023-12-30 18:46:15 +0400

    Update kde-preferences.sh

--- kde-preferences.sh
+++ kde-preferences.sh
@@ -1,4 +1,4 @@
-echo "deleting default media"
+echo "Deleting default media"
 sudo rm -r /usr/share/wallpapers/*
 sudo rm -r /usr/share/plasma/wallpapers/*
 sudo rm -r /usr/share/plasma/avatars/*
@@ -8,17 +8,16 @@ sudo mv media/* user/share/gitMedia
 echo "Deleting default sddm themes"
 sudo rm -r /usr/share/sddm/themes/*
 
-echo "Applying Nordic-bluish sddm theme"
-cd /usr/share/sddm
-sudo git clone https://github.com/EliverLara/Nordic.git
-sudo mv Nordic/kde/sddm/Nordic-bluish themes/Nordic-bluish
-sudo rm -r Nordic
-sudo sed -i "s/^#Current=.*/Current=Nordic-bluish/" /etc/sddm.conf.d/kde_settings.conf
-
 echo "Deleting default kde themes"
 sudo rm -r /usr/share/plasma/desktoptheme/*     # plasma style
 sudo rm -r /usr/share/plasma/look-and-feel/*    # global theme
 sudo rm -r /usr/share/color-schemes/*           # colours
+
+echo "Downloading Nordic-bluish sddm theme"
+cd /usr/share/sddm
+sudo git clone https://github.com/EliverLara/Nordic.git
+sudo mv Nordic/kde/sddm/Nordic-bluish themes/Nordic-bluish
+sudo rm -r Nordic
 
 echo "Downloading Dracula-purple kde theme"
 cd /usr/share/plasma
@@ -29,11 +28,18 @@ sudo mv gtk/kde/plasma/look-and-feel look-and-feel
 sudo mv gtk/kde/color-schemes /usr/share/color-schemes
 sudo rm -r gtk
 
-echo "Deleting default icons"
-
-
 echo "Downloading Dexy-Color-Dark icons"
 cd /usr/share/icons
 sudo git clone https://github.com/L4ki/Dexy-Plasma-Themes.git
 sudo mv Dexy-Plasma-Themes/Dexy-Color-Icons/Dexy-Color-Dark-Icons Dexy-Color-Dark-Icons
 sudo rm -r Dexy-Plasma-Themes
+
+# lockscreen config
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key Autolock false
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key Timeout 30
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key LockGrace 300
+kwriteconfig5 --file ~/.config/kscreenlockerrc --group Daemon --key LockOnResume false
+
+# Apply SDDM theme
+sudo bash -c 'echo "Theme" >> /etc/sddm.conf'
+sudo bash -c 'echo "Current=Nordic-bluish" >> /etc/sddm.conf'
