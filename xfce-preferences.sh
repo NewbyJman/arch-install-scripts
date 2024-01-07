e0b13e0d12f5486a0d790ab18cc3e825cd77091a
Jzaib <68151478+NewbyJman@users.noreply.github.com>
2024-01-07 17:25:19 +0400

    Update xfce-preferences.sh

--- xfce-preferences.sh
+++ xfce-preferences.sh
@@ -1,6 +1,6 @@
 # Import media
-sudo mkdir -p /home/zaib/.local/share/gitMedia
-sudo cp -f /home/zaib/arch-install-scripts/media/* /home/zaib/.local/share/gitMedia/
+sudo mkdir -p ~/.local/share/gitMedia
+sudo cp -f ~/arch-install-scripts/media/* ~/.local/share/gitMedia/
 
 # Configure SDDM
 echo "Downloading sddm theme"
@@ -11,11 +11,11 @@ sudo bash -c 'echo "[Theme]" >> /usr/lib/sddm/sddm.conf.d/sddm.conf'
 sudo bash -c 'echo "Current=sddm-chili" >> /usr/lib/sddm/sddm.conf.d/sddm.conf'
 sudo bash -c 'sed -i "s/^Current=.*/Current=sddm-chili/" /lib/sddm/sddm.conf.d/default.conf'
 sudo bash -c 'sed -i "s/^CursorTheme=.*/CursorTheme=Dracula-cursors/" /lib/sddm/sddm.conf.d/default.conf'
-sudo cp /home/zaib/.local/share/gitMedia/gintoki.png /usr/share/sddm/faces/zaib.face.icon
-sudo cp /home/zaib/.local/share/gitMedia/gintoki.png /home/zaib/.face
-sudo cp -f /home/zaib/.local/share/gitMedia/lockscreen.jpg /usr/share/sddm/themes/sddm-chili/assets/background.jpg
-sudo cp -f /home/zaib/arch-install-scripts/configs/sddm/svg/* /usr/share/sddm/themes/sddm-chili/assets/
-sudo cp -f /home/zaib/arch-install-scripts/configs/sddm/Main.qml /usr/share/sddm/themes/sddm-chili/
+sudo cp ~/.local/share/gitMedia/gintoki.png /usr/share/sddm/faces/zaib.face.icon
+sudo cp ~/.local/share/gitMedia/gintoki.png ~/.face
+sudo cp -f ~/.local/share/gitMedia/lockscreen.jpg /usr/share/sddm/themes/sddm-chili/assets/background.jpg
+sudo cp -f ~/arch-install-scripts/configs/sddm/svg/* /usr/share/sddm/themes/sddm-chili/assets/
+sudo cp -f ~/arch-install-scripts/configs/sddm/Main.qml /usr/share/sddm/themes/sddm-chili/
 
 # Download global theme, cursors, icons
 echo "Downloading Dracula-purple kde theme"
@@ -47,28 +47,27 @@ xfconf-query -c xfwm4 -p /general/snap_to_windows -s true
 xfconf-query -c xfwm4 -p /general/snap_width -s 10
 xfconf-query -c xfwm4 -p /general/wrap_windows -s false
 xfconf-query -c xfwm4 -p /general/wrap_workspaces -s false
-xfconf-query -c xfwm4 -p /general/wrap_resistance -s 10
 xfconf-query -c xfwm4 -p /general/workspace_count -s 1
+xfconf-query -c xfwm4 -p /general/wrap_resistance -s 10
 xfconf-query -c xfwm4 -p /general/double_click_action -s "maximize"
 
 # Configure desktop
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0 -n -t "string" -s "replace me"
-xfconf-query -c xfce4-desktop -p /desktop-icons -n -t "string" -s "replace me"
-xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons -n -t "string" -s "replace me"
-sed -i 's/type="string" value="replace me"/type="empty"/g' /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+# xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0 -n -t "string" -s "replace me"
+# xfconf-query -c xfce4-desktop -p /desktop-icons -n -t "string" -s "replace me"
+# xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons -n -t "string" -s "replace me"
+# sed -i 's/type="string" value="replace me"/type="empty"/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+# xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -n -t string -s ~/.local/share/gitMedia/wallpaper.png
+# xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-show -n -t bool -s true
+# xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style -n -t int -s 5
+# xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-home -n -t bool -s false
+# xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -n -t bool -s false
+# xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-trash -n -t bool -s false
+# xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-removable -n -t bool -s true
 sudo cp -f ~/arch-install-scripts/configs/nitrogen/* ~/.config/nitrogen/
 
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -n -t string -s /home/zaib/.local/share/gitMedia/wallpaper.png
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-show -n -t bool -s true
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style -n -t int -s 5
-xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-home -n -t bool -s false
-xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -n -t bool -s false
-xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-trash -n -t bool -s false
-xfconf-query -c xfce4-desktop -p /desktop-icons/file-icons/show-removable -n -t bool -s true
-
 # Docklike Taskbar
-mkdir -p /home/zaib/.config/xfce4/panel
-cat <<EOT >> /home/zaib/.config/xfce4/panel/docklike-2.rc
+mkdir -p ~/.config/xfce4/panel
+cat <<EOT >> ~/.config/xfce4/panel/docklike-2.rc
     [user]
     showPreviews=true
     indicatorStyle=1
@@ -81,24 +80,24 @@ EOT
 # Keymap mapping
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom -r -R
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom -n -t "string" -s "replace me"
-sed -i 's/type="string" value="replace me"/type="empty"/g' /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+sed -i 's/type="string" value="replace me"/type="empty"/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/override -n -t bool -s true
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"Print" -n -t string -s "flameshot gui"
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Super>p" -n -t string -s "xfce4-display-settings --minimal"
-xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Primary><Shift>Escape" -n -t string -s "xfce4-taskmanager"
+xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Primary><Shift>Escape" -n -t string -s "lxtask"
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Super>l" -n -t string -s "xfce4-session-logout"
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Primary>Super_L" -n -t string -s "xfce4-popup-whiskermenu"
-xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Alt>Return" -n -t string -s "xfce4-appfinder"
+xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Alt>Return" -n -t string -s "rofi -show dmenu"
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Super>e" -n -t string -s "nemo"
 xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Super>t" -n -t string -s "xfce4-terminal"
-xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Alt>b" -n -t string -s "chromium google.co.uk"
-sudo cp -f /home/zaib/arch-install-scripts/configs/parcelliterc /home/zaib/.config/parcellite/
+xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/"<Alt>b" -n -t string -s "chromium --incognito google.co.uk"
+sudo cp -f ~/arch-install-scripts/configs/parcelliterc ~/.config/parcellite/
 
 # power management and screensaver
 echo "screensaver"
 xfconf-query -c xfce4-screensaver -p /lock -n -t "string" -s "replace me"
 xfconf-query -c xfce4-screensaver -p /saver -n -t "string" -s "replace me"
-sed -i 's/type="string" value="replace me"/type="empty"/g' /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-screensaver.xml
+sed -i 's/type="string" value="replace me"/type="empty"/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-screensaver.xml
 xfconf-query -c xfce4-screensaver -p /lock/sleep-activation -n -t bool -s false
 xfconf-query -c xfce4-screensaver -p /lock/enabled -n -t bool -s false
 xfconf-query -c xfce4-screensaver -p /saver/mode -n -t int -s 0
@@ -108,13 +107,13 @@ echo "session management"
 xfconf-query -c xfce4-session -p /general/SaveOnExit -n -t bool -s false
 xfconf-query -c xfce4-session -p /sessions/shutdown -r -R
 xfconf-query -c xfce4-session -p /sessions/shutdown -n -t "string" -s "replace me"
-sed -i 's/type="string" value="replace me"/type="empty"/g' /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+sed -i 's/type="string" value="replace me"/type="empty"/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
 xfconf-query -c xfce4-session -p /sessions/shutdown/LockScreen -n -t bool -s false
 
 echo "power management"
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager -r -R
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager -n -t "string" -s "replace me"
-sed -i 's/type="string" value="replace me"/type="empty"/g' /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+sed -i 's/type="string" value="replace me"/type="empty"/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-store-restore-on-exit -n -t int -s 1
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-store -n -t bool -s false
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lock-screen-suspend-hibernate -n -t bool -s false
@@ -126,14 +125,14 @@ xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac -n -t in
 # Notifications
 echo "Notifications"
 xfconf-query -c xfce4-notifyd -p /plugin -n -t "string" -s "replace me"
-sed -i 's/type="string" value="replace me"/type="empty"/g' /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-notifyd.xml
+sed -i 's/type="string" value="replace me"/type="empty"/g' ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-notifyd.xml
 xfconf-query -c xfce4-notifyd -p /plugin/hide-on-read -n -t bool -s true
 xfconf-query -c xfce4-notifyd -p /plugin/log-icon-size -n -t int -s 15
 xfconf-query -c xfce4-notifyd -p /plugin/log-only-today -n -t bool -s true
 xfconf-query -c xfce4-notifyd -p /log-max-size-enabled -n -t bool -s true
 
 # Mouse and touchpad
-cat <<EOT >> /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
+cat <<EOT >> ~/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
     <?xml version="1.0" encoding="UTF-8"?>
 
     <channel name="pointers" version="1.0">
@@ -146,14 +145,18 @@ cat <<EOT >> /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/pointers.xml
     </channel>
 EOT
 
-# appfinder
-echo "appfinder"
-xfconf-query -c xfce4-appfinder -p /hide-window-decorations -n -t bool -s true
-xfconf-query -c xfce4-appfinder -p /hide-category-pane -n -t bool -s true
-xfconf-query -c xfce4-appfinder -p /icon-view -n -t bool -s true
-xfconf-query -c xfce4-appfinder -p /sort-by-frecency -n -t bool -s true
-xfconf-query -c xfce4-appfinder -p /single-window -n -t bool -s true
-xfconf-query -c xfce4-appfinder -p /text-beside-icons -n -t bool -s false
+# /us/share/X11/xorg.conf.d/40-libinput.conf
+# option "VertScrollDelta" "-100"
+# option "HorizScrollDelta" "-100"
+# option "TapButton1" "1"
+# option "TapButton2" "3"
+
+# rofi
+echo "rofi"
+sudo rm -rf /usr/share/rofi/themes/*
+sudo cp ~/arch-install-scripts/configs/zaib.rasi /usr/share/rofi/themes/
+sudo mkdir -p ~/.config/rofi
+sudo echo '@theme "/usr/share/rofi/themes/zaib.rasi"' >> ~/.config/rofi/config.rasi
 
 # Terminal
 echo "terminal"
@@ -172,7 +175,7 @@ xfconf-query -c xfce4-terminal -p /color-background -n -t string -s "#0b840068da
 xfconf-query -c xfce4-terminal -p /tab-activity-color -n -t string -s "1c1c7171d8d8"
 
 # Import remaining conf files
-sudo cp -f /home/zaib/arch-install-scripts/configs/xfce4-panel.xml /home/zaib/.config/xfce4/xfconf/xfce-perchannel-xml/
-sudo cp -f /home/zaib/arch-install-scripts/configs/startup/* /etc/xdg/autostart/
+sudo cp -f ~/arch-install-scripts/configs/xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+sudo cp -f ~/arch-install-scripts/configs/startup/* /etc/xdg/autostart/
 
-sudo rm -r /home/zaib/arch-install-scripts
+# sudo rm -r ~/arch-install-scripts
